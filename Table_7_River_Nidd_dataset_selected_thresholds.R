f4160f4a33dd4d7856cee28aeb99512ac2544cca
Zak D Varty <44709288+zakvarty@users.noreply.github.io>
2024-09-16 14:01:44 +0100

    tidy table 7 code

--- Table_7_River_Nidd_dataset_selected_thresholds.R
+++ Table_7_River_Nidd_dataset_selected_thresholds.R
@@ -1,35 +1,60 @@
+#===============================================================================
+# Code to produce the estimates shown in Table 7 of the main paper. 
+#===============================================================================
+
+SEED = 11111
+OUTPUT_PATH = "output/tables/Table_7_River_Nidd_dataset_selected_thresholds.csv"
 
 library(evir)
+library(threshr)
+library(ismev)
+library(mgcv)
+library(nlme)
 source("src/eqd.R")
 source("src/JointMLEFunctions.r")
-library(threshr)
 
-data("nidd.thresh")
+# Table 7:  --------------------------------------------------------------------
 
-# Table 7:  -----------------------------------------------------
+# Load Nidd data
+data("nidd.thresh",package = "evir")
 
-#Candidate threshold grids
-thresholds_list <- list(quantile(nidd.thresh, seq(0,0.93, by=0.01)),quantile(nidd.thresh, seq(0,0.9, by=0.01)), quantile(nidd.thresh, seq(0,0.8, by=0.01)), 
-                        quantile(nidd.thresh, seq(0,0.8, by=0.2)), quantile(nidd.thresh, seq(0,0.9, by=0.3)), quantile(nidd.thresh, seq(0,0.75, by=0.25)),
-                        quantile(nidd.thresh, c(0,0.1,0.4,0.7)))
+# Define candidate threshold grids
+thresholds_list <- list(
+  quantile(nidd.thresh, seq(from = 0, to = 0.93, by = 0.01)),
+  quantile(nidd.thresh, seq(from = 0, to = 0.90, by = 0.01)),
+  quantile(nidd.thresh, seq(from = 0, to = 0.80, by = 0.01)), 
+  quantile(nidd.thresh, seq(from = 0, to = 0.80, by = 0.20)),
+  quantile(nidd.thresh, seq(from = 0, to = 0.90, by = 0.30)),
+  quantile(nidd.thresh, seq(from = 0, to = 0.75, by = 0.25)),
+  quantile(nidd.thresh, c(0, 0.10, 0.40, 0.70)))
 
-EQD_thr <- wads_thr <- north_thr <- numeric(7)
-for(i in 1:7){
-  print(i)
+# Select threshold using each method-grid combination --------------------------
+
+EQD_thr <- wads_thr <- north_thr <- numeric(length = length(thresholds_list))
+
+for (i in seq_along(thresholds_list)) {
+  
+  cat(paste0("Starting threshold selection with candidate set ", i, " of 7.\n"))
   thresholds <- thresholds_list[[i]]
-  #EQD
-  set.seed(11111) 
-  EQD_thr[i] <- eqd(nidd.thresh, thresholds, k=200)$thresh
-  #Wadsworth
-  set.seed(11111)
-  wads_thr[i] <- NHPP.diag(nidd.thresh, u=thresholds, plot.out=FALSE, UseQuantiles = FALSE)$thresh
-  #Northrop
-  set.seed(11111)
+  
+  cat("\t 1/3 selecting threshold using EQD method \n")
+  set.seed(SEED) 
+  EQD_thr[i] <- eqd(nidd.thresh, thresholds, k = 200)$thresh
+  
+  cat("\t 2/3 selecting threshold using Wadsworth method \n")
+  set.seed(SEED)
+  wads_thr[i] <- NHPP.diag(
+    x = nidd.thresh,
+    u = thresholds,
+    plot.out = FALSE,
+    UseQuantiles = FALSE)$thresh
+  
+  cat("\t 3/3 selecting threshold using Northrop method \n")
+  set.seed(SEED)
   north_thr[i] <- summary(ithresh(nidd.thresh, u_vec = thresholds))[3]
 }
 
-table_7 <- data.frame(EQD=EQD_thr, Wadsworth=wads_thr, Northrop=north_thr)
-
+table_7 <- data.frame(EQD = EQD_thr, Wadsworth = wads_thr, Northrop = north_thr)
 print(table_7)
 
-write.csv(table_7, "output/tables/Table_7_River_Nidd_dataset_selected_thresholds.csv")
+write.csv(table_7, OUTPUT_PATH)
